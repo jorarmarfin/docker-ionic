f0456c21791ade02d464a4f6cfb57c68369c7890
Luis mayta <luis.mayta@gmail.com>
2019-08-21 14:53:55 -0500

    version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build -t jorarmarfin/ionic:tmp .
-docker build -t jorarmarfin/ionic:0.1 .
+docker build -t jorarmarfin/ionic:5.2.5 .
 
 docker run --name srv-ionic -dit jorarmarfin/ionic:0.1
 
